386eb89e569bde6ec4a38e2fc42ea45ae47d1498
Tom Brown <tom@nworbmot.org>
2018-02-17 16:39:16 +0100

    Load data dynamically and switch between different scenarios
    Python export code is also more modular

--- export_network.py
+++ export_network.py
@@ -1,22 +1,10 @@
-import pypsa, json
+import pypsa, json, os
 
 import pandas as pd
 
 #%matplotlib inline
 
-network_name = "/home/tom/results/tom/snakemake/version-16/postnetworks/postnetwork-elec_only_opt.h5"
-
-network_name = "/home/tom/results/supplementary_data_benefits_of_cooperation/results/diw2030-CO0-T1_8761-wWsgrpHb-LV0.25_c0_base_diw2030_solar1_7_angles-2017-01-31-20-12-02/"
-
-n = pypsa.Network(network_name)
-
-coord_round = 3
-
-power_round = 0
-
-num_snapshots = 168
-
-folder = "./"
+preferred_order = pd.Index(["offwind","onwind","solar","OCGT","ror","hydro","PHS","battery","H2"])
 
 colors = {"OCGT" :"#835C3B",
           "onwind":"#3B6182",
@@ -51,91 +39,116 @@ def rename_techs(label):
         label = "gas OCGT"
     return label
 
-#corrections to improve optics
 
-n.buses.loc["NO",["x","y"]] = [9.5,61.5]
-n.buses.loc["SE",["x","y"]] = [15,60.5]
-n.buses.at["FI","y"] = 62
+#convert MW to GW
+factor = 1e3
 
-carrier = "AC"
+def export_network_to_json(network, export_folder, snapshots=None, coord_round=3, power_round=1):
+    """power_round refers to GW of power"""
+    
+    n = network
+    folder = export_folder
+    
+    if not os.path.isdir(folder):
+        print("Directory {} does not exist, creating it"
+                           .format(folder))
+        os.mkdir(folder)
 
-buses = n.buses[n.buses.carrier == carrier]
+    
+    if snapshots is None:
+        snapshots = n.snapshots
 
-data = {"index" : list(buses.index),
-        "x" : list(buses.x.round(coord_round)),
-        "y" : list(buses.y.round(coord_round)),
+    with open(folder + 'snapshots.json', 'w') as fp:
+        json.dump([str(i) for i in snapshots], fp)
+
+    #corrections to improve optics
+    n.buses.loc["NO",["x","y"]] = [9.5,61.5]
+    n.buses.loc["SE",["x","y"]] = [15,60.5]
+    n.buses.at["FI","y"] = 62
+    
+    #only take AC buses
+    carrier="AC"
+    buses = n.buses[n.buses.carrier == carrier]
+    
+    data = {"index" : list(buses.index),
+            "x" : list(buses.x.round(coord_round)),
+            "y" : list(buses.y.round(coord_round)),
+           }
+
+
+    with open(folder + 'buses.json', 'w') as fp:
+        json.dump(data, fp)
+    
+    #select links between relevant buses
+    carrier0 = (n.links.bus0.map(n.buses.carrier) == "AC")
+    carrier1 = (n.links.bus1.map(n.buses.carrier) == "AC")
+
+    links = n.links[carrier0 & carrier1]
+    
+    
+    data = {"index" : list(links.index),
+            "bus0" : list(links.bus0),
+            "bus1" : list(links.bus1),
+            "x0" : list(links.bus0.map(buses.x).round(coord_round)),
+            "y0" : list(links.bus0.map(buses.y).round(coord_round)),
+            "x1" : list(links.bus1.map(buses.x).round(coord_round)),
+            "y1" : list(links.bus1.map(buses.y).round(coord_round)),
+            "p_nom_opt" : list(links.p_nom_opt.round(power_round)),
        }
 
 
-with open(folder + 'buses.json', 'w') as fp:
-    fp.write("var buses = ")
-    json.dump(data, fp)
+    with open(folder + 'links.json', 'w') as fp:
+        json.dump(data, fp)
+        
+        
+    with open(folder + 'flow.json', 'w') as fp:
+        json.dump((n.links_t.p0.reindex(columns=links.index).fillna(0.).loc[snapshots]/factor).round(power_round).values.tolist(),fp)
+        
+        
+    generation_carriers = n.generators.carrier.value_counts().index
+    generation_carriers = (preferred_order&generation_carriers).append(generation_carriers.difference(preferred_order))
+    print("generation carriers:",generation_carriers)
+    
+    storage_carriers = n.storage_units.carrier.value_counts().index
+    storage_carriers = (preferred_order&storage_carriers).append(storage_carriers.difference(preferred_order))
+    print("storage carriers:",storage_carriers)
+    
+    carriers = {}
 
-links = n.links[n.links.index.str[2:3] == "-"]
+    carriers["positive"] = generation_carriers.append(storage_carriers)
 
-links.bus0.map(buses.x)
+    carriers["negative"] = pd.Index(["load"]).append(storage_carriers)
 
-data = {"index" : list(links.index),
-        "bus0" : list(links.bus0),
-        "bus1" : list(links.bus1),
-        "x0" : list(links.bus0.map(buses.x).round(coord_round)),
-        "y0" : list(links.bus0.map(buses.y).round(coord_round)),
-        "x1" : list(links.bus1.map(buses.x).round(coord_round)),
-        "y1" : list(links.bus1.map(buses.y).round(coord_round)),
-        "p_nom_opt" : list(links.p_nom_opt.round(power_round)),
-       }
-
-
-with open(folder + 'links.json', 'w') as fp:
-    fp.write("var links = ")
-    json.dump(data, fp)
-
-with open(folder + 'flow.json', 'w') as fp:
-    fp.write("var flows = ")
-    json.dump(n.links_t.p0[links.index][:num_snapshots].round(power_round).values.tolist(),fp)
-
-
-with open(folder + 'snapshots.json', 'w') as fp:
-    fp.write("var snapshots= ")
-    json.dump([str(i) for i in n.snapshots[:num_snapshots]], fp)
-
-preferred_order = pd.Index(["offwind","onwind","solar","OCGT","ror","hydro","PHS","battery","H2"])
-
-generation_carriers = n.generators.carrier.value_counts().index
-generation_carriers = (preferred_order&generation_carriers).append(generation_carriers.difference(preferred_order))
-generation_carriers
-
-storage_carriers = n.storage_units.carrier.value_counts().index
-storage_carriers = (preferred_order&storage_carriers).append(storage_carriers.difference(preferred_order))
-storage_carriers
-
-carriers = {}
-
-carriers["positive"] = generation_carriers.append(storage_carriers)
-
-carriers["negative"] = pd.Index(["load"]).append(storage_carriers)
-print(carriers)
-
-with open(folder + 'carriers.json', 'w') as fp:
-        fp.write("var carriers = ")
+    with open(folder + 'carriers.json', 'w') as fp:
         json.dump({sign : {"index" : [rename_techs(c) for c in carriers[sign]],
                    "color" : [colors[c] for c in carriers[sign]]} for sign in ["positive","negative"]}, fp)
 
-data = {"positive" : [],
-        "negative" : []}
-for ct in buses.index:
+    data = {"positive" : [],
+            "negative" : []}
 
-    storage = n.storage_units_t.p.loc[:,n.storage_units.bus == ct].groupby(n.storage_units.carrier,axis=1).sum().reindex(columns=storage_carriers).fillna(0.)
-    generation = n.generators_t.p.loc[:,n.generators.bus == ct].groupby(n.generators.carrier,axis=1).sum().reindex(columns=generation_carriers).fillna(0.)
-    load = n.loads_t.p_set.loc[:,n.loads.bus == ct].sum(axis=1)
-    load.name = "load"
+    for ct in buses.index:
+
+        storage = n.storage_units_t.p.loc[:,n.storage_units.bus == ct].groupby(n.storage_units.carrier,axis=1).sum().reindex(columns=storage_carriers).fillna(0.)
+        generation = n.generators_t.p.loc[:,n.generators.bus == ct].groupby(n.generators.carrier,axis=1).sum().reindex(columns=generation_carriers).fillna(0.)
+        load = n.loads_t.p_set.loc[:,n.loads.bus == ct].sum(axis=1)
+        load.name = "load"
     
-    data["positive"].append(pd.concat((generation,storage[storage > 0]),axis=1).fillna(0.)[:num_snapshots].round(power_round).values.tolist())
-    data["negative"].append(pd.concat((load,-storage[storage < 0]),axis=1).fillna(0.)[:num_snapshots].round(power_round).values.tolist())
+        data["positive"].append((pd.concat((generation,storage[storage > 0]),axis=1).fillna(0.)/factor).loc[snapshots].round(power_round).values.tolist())
+        data["negative"].append((pd.concat((load,-storage[storage < 0]),axis=1).fillna(0.)/factor).loc[snapshots].round(power_round).values.tolist())
 
 
-with open(folder + 'power.json', 'w') as fp:
-    fp.write("var power = ")
-    json.dump(data,fp)
+    with open(folder + 'power.json', 'w') as fp:
+        json.dump(data,fp)
+
+to_export = {0 : "/home/tom/results/supplementary_data_benefits_of_cooperation/results/diw2030-CO0-T1_8761-wWsgrpHb-LV0.0_c0_base_diw2030_solar1_7_angles-2017-01-31-20-12-02/",
+             1 : "/home/tom/results/supplementary_data_benefits_of_cooperation/results/diw2030-CO0-T1_8761-wWsgrpHb-LV0.0625_c0_base_diw2030_solar1_7_angles-2017-02-13-18-51-06/",
+             4 : "/home/tom/results/supplementary_data_benefits_of_cooperation/results/diw2030-CO0-T1_8761-wWsgrpHb-LV0.25_c0_base_diw2030_solar1_7_angles-2017-01-31-20-12-02/"}
+
+             
+for k,v in to_export.items():
+    n = pypsa.Network(v)
+    
+    export_network_to_json(n,"./" + str(k) + "/",snapshots=n.snapshots[:168])
+
 
 
--- network.js
+++ network.js
@@ -28,9 +28,9 @@ var w = 500;
 var h = 500;
 
 
-//Scale links
-var flow_scale = 1000;
-var power_scale = 10;
+//Scale flow and power
+var flow_scale = 1;
+var power_scale = 0.3;
 
 
 
@@ -49,12 +49,6 @@ var path = d3.geo.path()
     .projection(projection);
 
 
-document.getElementById("timeslide").max=snapshots.length-1;
-
-document.getElementById("timeslide").value = snapshot_index;
-
-document.getElementById("range").innerHTML=snapshots[snapshot_index];
-
 
 //Create SVG
 var svg = d3.select("#right")
@@ -65,22 +59,65 @@ var svg = d3.select("#right")
 var signs = ["positive","negative"];
 
 
+var files_to_load = ["snapshots","carriers","power","flow","buses","links"];
+
+var scenario = 4;
+
+var network = {};
 
 
-//Load in GeoJSON data
-d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
+//recursively load files into network object
+//function after is executed at the end
+
+function load_data(scenario, after){
+
+    var k = 0;
+
+    function load_next(){
+
+	if(k >= files_to_load.length){
+	    after();
+	    return;
+	}
+
+	var file_name = scenario + "/" + files_to_load[k] + ".json";
+
+	d3.json(file_name, function(data){
+	    network[files_to_load[k]] = data;
+	    k++;
+	    load_next();
+	});
+    }
+
+    load_next();
+}
+
+
+//execute this function once all the data is loaded
+
+function display_data(){
+
+    document.getElementById("timeslide").max=network.snapshots.length-1;
+
+    document.getElementById("timeslide").value = snapshot_index;
+
+    document.getElementById("range").innerHTML=network.snapshots[snapshot_index];
 
 
     //Legend
     for(var k=0; k < signs.length; k++) {
 	var sign=signs[k];
+
+	// remove existing
+	d3.select("#legend-" + sign).selectAll("svg").remove();
+
 	var legendSVG = d3.select("#legend-" + sign)
 	    .append("svg")
 	    .attr("width",180)
-	    .attr("height",carriers[sign].index.length*20);
+	    .attr("height",network.carriers[sign].index.length*20);
 
 	var legend = legendSVG.selectAll("g")
-	    .data(carriers[sign].index)
+	    .data(network.carriers[sign].index)
 	    .enter()
 	    .append("g")
 	    .attr("transform", function (d, i) {  return "translate(0," + (5 + i * 20) + ")" });
@@ -90,7 +127,7 @@ d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
 	    .attr("y",0)
 	    .attr("width", 10)
 	    .attr("height", 10)
-	    .style("fill", function (d, i) { return carriers[sign].color[i] });
+	    .style("fill", function (d, i) { return network.carriers[sign].color[i] });
 
 	legend.append("text")
 	    .attr("x",20)
@@ -98,39 +135,33 @@ d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
 	    .text(function (d) { return d});
     }
 
+    // remove existing
+    d3.select("#legend-scale").selectAll("svg").remove();
+
     var legendSVG = d3.select("#legend-scale")
 	.append("svg")
         .attr("width",180)
         .attr("height",120);
 
-    legendSVG.append("circle").attr("cx",20).attr("cy",15).attr("r",5000**0.5/power_scale).attr("fill","#FFFFFF").attr("stroke","black").attr("stroke-width",1);
+    legendSVG.append("circle").attr("cx",20).attr("cy",15).attr("r",5**0.5/power_scale).attr("fill","#FFFFFF").attr("stroke","black").attr("stroke-width",1);
 
     legendSVG.append("text").attr("x",40).attr("y",20).text("5 GW");
 
-    legendSVG.append("circle").attr("cx",20).attr("cy",45).attr("r",25000**0.5/power_scale).attr("fill","#FFFFFF").attr("stroke","black").attr("stroke-width",1);
+    legendSVG.append("circle").attr("cx",20).attr("cy",45).attr("r",25**0.5/power_scale).attr("fill","#FFFFFF").attr("stroke","black").attr("stroke-width",1);
 
     legendSVG.append("text").attr("x",40).attr("y",50).text("25 GW");
 
-    legendSVG.append("rect").attr("x",0).attr("y",70).attr("width",30).attr("height",1000/flow_scale).attr("fill","#999999");
+    legendSVG.append("rect").attr("x",0).attr("y",70).attr("width",30).attr("height",1/flow_scale).attr("fill","#999999");
 
     legendSVG.append("text").attr("x",40).attr("y",80).text("1 GW flow");
 
-    legendSVG.append("rect").attr("x",0).attr("y",90).attr("width",30).attr("height",10000/flow_scale).attr("fill","#999999");
+    legendSVG.append("rect").attr("x",0).attr("y",90).attr("width",30).attr("height",10/flow_scale).attr("fill","#999999");
 
     legendSVG.append("text").attr("x",40).attr("y",100).text("10 GW flow");
 
-    countries = svg.append("g")
-        .attr("id","countries");
 
-    //Bind data and create one path per GeoJSON feature
-    countries.selectAll("path")
-	.data(json.features)
-	.enter()
-	.append("path")
-	.attr("d", path)
-	.attr("stroke", "rgba(8, 81, 156, 0.2)")
-	.attr("fill", "rgba(8, 81, 156, 0.01)");
-
+    // remove existing
+    d3.select("g#lines").remove();
 
     line_layer = svg.append("g")
         .attr("id","lines");
@@ -142,12 +173,12 @@ d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
         .interpolate("linear");
 
     lines = line_layer.selectAll("path")
-	.data(links.index)
+	.data(network.links.index)
 	.enter()
 	.append("path")
-	.attr("d", function(d, i) {var from = 0; if(flows[snapshot_index][i] < 0){from = 1}; return lineFunction([projection([links["x" + from][i],links["y" + from][i]]),projection([links["x" + (1-from)][i],links["y" + (1-from)][i]])])})
+	.attr("d", function(d, i) {var from = 0; if(network.flow[snapshot_index][i] < 0){from = 1}; return lineFunction([projection([network.links["x" + from][i],network.links["y" + from][i]]),projection([network.links["x" + (1-from)][i],network.links["y" + (1-from)][i]])])})
         .attr("class", "flowline-animated")
-        .attr("stroke-width", function(d, i) { return Math.abs(flows[snapshot_index][i])/flow_scale});
+        .attr("stroke-width", function(d, i) { return Math.abs(network.flow[snapshot_index][i])/flow_scale});
 
 
     // This is a function which transforms arc data into a path
@@ -168,14 +199,17 @@ d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
 
 	sign = signs[k];
 
+	// remove existing
+	d3.select("g#"+sign).remove();
+
 	sign_layer[sign] = svg.append("g").attr("id",sign);
 
 
 	sign_locations[sign] = sign_layer[sign].selectAll("g")
-            .data(power[sign])
+            .data(network.power[sign])
             .enter()
             .append("g")
-            .attr("transform", function(d,i) { return "translate(" + projection([buses.x[i],buses.y[i]])[0] +","+ projection([buses.x[i],buses.y[i]])[1] + ")" } );
+            .attr("transform", function(d,i) { return "translate(" + projection([network.buses.x[i],network.buses.y[i]])[0] +","+ projection([network.buses.x[i],network.buses.y[i]])[1] + ")" } );
 
 
 
@@ -185,10 +219,41 @@ d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
         .append("path")
         .attr("d", function(d) { return arc_path.outerRadius(d["total"]**0.5/power_scale)(d)})
 	    .attr("class",sign)
-        .style("fill", function(d, i) { return carriers[sign].color[i] });
+        .style("fill", function(d, i) { return network.carriers[sign].color[i] });
 
     };
+}
 
+
+
+//Load in GeoJSON data
+d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
+
+    countries = svg.append("g")
+        .attr("id","countries");
+
+    //Bind data and create one path per GeoJSON feature
+    countries.selectAll("path")
+	.data(json.features)
+	.enter()
+	.append("path")
+	.attr("d", path)
+	.attr("stroke", "rgba(8, 81, 156, 0.2)")
+	.attr("fill", "rgba(8, 81, 156, 0.01)");
+
+
+
+    load_data(scenario, display_data);
+});
+
+
+
+// when the scenario changes, reload the data
+
+d3.selectAll("input[name='scenario']").on("change", function(){
+    scenario = this.value;
+    console.log("scenario changed to", scenario);
+    load_data(scenario, display_data);
 });
 
 
@@ -200,11 +265,11 @@ d3.select("#timeslide").on("input", function() {
 
 function update(value) {
     snapshot_index = value;
-    document.getElementById("range").innerHTML=snapshots[snapshot_index];
+    document.getElementById("range").innerHTML=network.snapshots[snapshot_index];
 
     line_layer.selectAll("path")
-	.attr("d", function(d, i) {var from = 0; if(flows[snapshot_index][i] < 0){from = 1}; return lineFunction([projection([links["x" + from][i],links["y" + from][i]]),projection([links["x" + (1-from)][i],links["y" + (1-from)][i]])])})
-        .attr("stroke-width", function(d, i) { return Math.abs(flows[snapshot_index][i])/flow_scale});
+	.attr("d", function(d, i) {var from = 0; if(network.flow[snapshot_index][i] < 0){from = 1}; return lineFunction([projection([network.links["x" + from][i],network.links["y" + from][i]]),projection([network.links["x" + (1-from)][i],network.links["y" + (1-from)][i]])])})
+        .attr("stroke-width", function(d, i) { return Math.abs(network.flow[snapshot_index][i])/flow_scale});
 
     for(var k=0; k < signs.length; k++) {
 
@@ -214,7 +279,7 @@ function update(value) {
 	sign_locations[sign].selectAll("path")
             .data(function(d) {return half_pie(d[snapshot_index], startAngle[sign])})
         .attr("d", function(d) { return arc_path.outerRadius(d["total"]**0.5/power_scale)(d)})
-        .style("fill", function(d, i) { return carriers[sign].color[i] });
+        .style("fill", function(d, i) { return network.carriers[sign].color[i] });
     }
 }
 
@@ -234,7 +299,7 @@ playButton
 	    // timer = 0;
 	    button.text("Play");
 	} else {
-	    if(snapshot_index == snapshots.length-1){
+	    if(snapshot_index == network.snapshots.length-1){
 		snapshot_index = -1;
 	    }
 	    moving = true;
@@ -247,7 +312,7 @@ playButton
 
 function step() {
 
-    if(snapshot_index == snapshots.length-1){
+    if(snapshot_index == network.snapshots.length-1){
 	moving = false;
 	clearInterval(timer);
 	// timer = 0;
