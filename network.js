f5bb1fea63f7c9530060dd877c907a58596de725
Tom Brown <tom@nworbmot.org>
2018-02-17 12:43:03 +0100

    Add power/flow scale

--- network.js
+++ network.js
@@ -30,15 +30,12 @@ var h = 500;
 
 //Scale links
 var link_scale = 1000;
-var power_scale = 10;
+var flow_scale = 10;
 
 
 
-//Index at start
-var start_snapshot_index = 4;
-
-//Keep track of index
-var snapshot_index = start_snapshot_index;
+//Current snapshot index; value sets start point
+var snapshot_index = 0;
 
 //Define map projection
 
@@ -74,8 +71,6 @@ var signs = ["positive","negative"];
 d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
 
 
-
-
     //Legend
     for(var k=0; k < signs.length; k++) {
 	var sign=signs[k];
@@ -102,6 +97,27 @@ d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
 	    .attr("y",10)
 	    .text(function (d) { return d});
     }
+
+    var legendSVG = d3.select("#legend-scale")
+	.append("svg")
+        .attr("width",180)
+        .attr("height",120);
+
+    legendSVG.append("circle").attr("cx",20).attr("cy",15).attr("r",5000**0.5/flow_scale).attr("fill","#FFFFFF").attr("stroke","black").attr("stroke-width",1);
+
+    legendSVG.append("text").attr("x",40).attr("y",20).text("5 GW");
+
+    legendSVG.append("circle").attr("cx",20).attr("cy",45).attr("r",25000**0.5/flow_scale).attr("fill","#FFFFFF").attr("stroke","black").attr("stroke-width",1);
+
+    legendSVG.append("text").attr("x",40).attr("y",50).text("25 GW");
+
+    legendSVG.append("rect").attr("x",0).attr("y",70).attr("width",30).attr("height",1000/link_scale).attr("fill","#999999");
+
+    legendSVG.append("text").attr("x",40).attr("y",80).text("1 GW flow");
+
+    legendSVG.append("rect").attr("x",0).attr("y",90).attr("width",30).attr("height",10000/link_scale).attr("fill","#999999");
+
+    legendSVG.append("text").attr("x",40).attr("y",100).text("10 GW flow");
 
     countries = svg.append("g")
         .attr("id","countries");
@@ -131,7 +147,7 @@ d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
 	.append("path")
 	.attr("d", function(d, i) { return lineFunction([projection([links.x0[i],links.y0[i]]),projection([links.x1[i],links.y1[i]])])})
         .attr("class", "flowline")
-        .attr("stroke-width", function(d, i) { return flows[start_snapshot_index][i]/link_scale});
+        .attr("stroke-width", function(d, i) { return flows[snapshot_index][i]/link_scale});
 
 
     // This is a function which transforms arc data into a path
@@ -164,10 +180,10 @@ d3.json("ne_50m_admin_0_countries_simplified.json", function(json) {
 
 
     sign_locations[sign].selectAll("path")
-            .data(function(d) {return half_pie(d[start_snapshot_index], startAngle[sign])})
+            .data(function(d) {return half_pie(d[snapshot_index], startAngle[sign])})
         .enter()
         .append("path")
-        .attr("d", function(d) { return arc_path.outerRadius(d["total"]**0.5/power_scale)(d)})
+        .attr("d", function(d) { return arc_path.outerRadius(d["total"]**0.5/flow_scale)(d)})
 	    .attr("class",sign)
         .style("fill", function(d, i) { return carriers[sign].color[i] });
 
@@ -196,7 +212,7 @@ function update(value) {
 	// don't need enter() and append() here...
 	sign_locations[sign].selectAll("path")
             .data(function(d) {return half_pie(d[value], startAngle[sign])})
-        .attr("d", function(d) { return arc_path.outerRadius(d["total"]**0.5/power_scale)(d)})
+        .attr("d", function(d) { return arc_path.outerRadius(d["total"]**0.5/flow_scale)(d)})
         .style("fill", function(d, i) { return carriers[sign].color[i] });
     }
 }
